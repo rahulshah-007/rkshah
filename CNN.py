5a3138c62cdc3042ebd8c69a02433a18d95a1b2c
rahulshah-007 <66476062+rahulshah-007@users.noreply.github.com>
2020-06-05 18:19:05 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -33,7 +33,7 @@ test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg',target_
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = clf.predict(test_image)
-training_set.class_indices
+train_set.class_indices
 if result[0][0] == 1:
     prediction = 'dog'
 else:
